6adde601def0468c5df60cc31fdca667cf5cc7ff
Dycha <daveheseling@gmail.com>
2018-02-24 16:55:00 +0100

    Added Vue to project

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Marcin Dyszkiewicz">
 
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>Art Blog @yield('title')</title>
 
     {{--Fonts--}}
